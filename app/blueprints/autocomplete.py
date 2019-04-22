191a34319e22a0f0e519d267df9f09d655d86c50
Alex Tomberg <a.tomberg@gmail.com>
2019-04-21 20:52:17 -0400

    Added API reference to readme and small fixes

--- app/blueprints/autocomplete.py
+++ app/blueprints/autocomplete.py
@@ -2,11 +2,11 @@ from flask import Blueprint, request, jsonify
 from models.transaction import guess_category
 
 
-bp = Blueprint('autocomplete', __name__)
+bp = Blueprint('autocomplete', __name__, url_prefix='/autocomplete')
 
 
-@bp.route('/guess/<string:what>', methods=['GET'])
-def guess(what):
+@bp.route('/<string:what>', methods=['GET'])
+def autocomplete(what):
     if what == 'category':
         q = request.values.get('q')
         return jsonify(dict(category=guess_category(q)))
