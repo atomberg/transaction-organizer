26f98bac3134c8a3e6224c1cc4d2fa75581767fb
Alex Tomberg <a.tomberg@gmail.com>
2019-04-22 21:09:17 -0400

    fixed autocomplete

--- app/blueprints/autocomplete.py
+++ app/blueprints/autocomplete.py
@@ -1,14 +1,25 @@
 from flask import Blueprint, request, jsonify
-from models.transaction import guess_category
+from models.transaction import guess_category, get_categories, get_suppliers
 
 
 bp = Blueprint('autocomplete', __name__, url_prefix='/autocomplete')
 
 
-@bp.route('/<string:what>', methods=['GET'])
-def autocomplete(what):
-    if what == 'category':
-        q = request.values.get('q')
-        return jsonify(dict(category=guess_category(q)))
-    else:
-        return jsonify(dict())
+@bp.route('/supplier', methods=['GET'])
+def supplier():
+    q = request.values.get('q')
+    items = [s for s in get_suppliers() if q in s]
+    return jsonify(items)
+
+
+@bp.route('/category', methods=['GET'])
+def category():
+    q = request.values.get('q')
+    items = [s for s in get_categories() if q in s]
+    return jsonify(items)
+
+
+@bp.route('/category/guess', methods=['GET'])
+def category_guess():
+    q = request.values.get('q')
+    return jsonify(dict(category=guess_category(q)))
