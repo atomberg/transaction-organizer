cec57a80057d1bba794285ca68f0a8e8c37f8f13
Alex Tomberg <a.tomberg@gmail.com>
2019-05-20 09:07:13 -0400

    Added currency formatter, WIP

--- app/server.py
+++ app/server.py
@@ -1,15 +1,22 @@
-from blueprints import autocomplete_bp, tables_bp, transactions_bp
+# from blueprints import autocomplete_bp, tables_bp, transactions_bp
+from blueprints import transactions_bp, persons_bp
 from flask import Flask
-from gevent.pywsgi import WSGIServer
+# from gevent.pywsgi import WSGIServer
 
 backend = Flask(__name__)
 
-backend.register_blueprint(autocomplete_bp)
-backend.register_blueprint(tables_bp)
+# backend.register_blueprint(autocomplete_bp)
+# backend.register_blueprint(tables_bp)
+backend.register_blueprint(persons_bp)
 backend.register_blueprint(transactions_bp)
 
 
+@backend.template_filter()
+def currency_format(value):
+    return f'{value:.2f}'
+
+
 if __name__ == "__main__":
-    # backend.run(host='localhost', port=5555, debug=True)
-    http_server = WSGIServer(('localhost', 5555), backend)
-    http_server.serve_forever()
+    backend.run(host='localhost', port=5555, debug=True)
+    # http_server = WSGIServer(('localhost', 5555), backend)
+    # http_server.serve_forever()
