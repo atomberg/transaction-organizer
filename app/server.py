ad8ffce7346a9df71c3bccfff3a05b22cbcffddb
Alex Tomberg <a.tomberg@gmail.com>
2019-06-01 21:52:08 -0400

    hardening the UI

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 from blueprints import transactions_bp, persons_bp
 from flask import Flask
-# from gevent.pywsgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 backend = Flask(__name__)
 
@@ -9,6 +9,6 @@ backend.register_blueprint(transactions_bp)
 
 
 if __name__ == "__main__":
-    backend.run(host='localhost', port=5555, debug=True)
-    # http_server = WSGIServer(('localhost', 5555), backend)
-    # http_server.serve_forever()
+    # backend.run(host='localhost', port=5555, debug=True)
+    http_server = WSGIServer(('localhost', 5555), backend)
+    http_server.serve_forever()
