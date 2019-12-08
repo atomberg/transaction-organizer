1f93418563a1015ab3885ed3aa965a9ae95c441c
Alex Tomberg <a.tomberg@gmail.com>
2019-12-07 19:31:36 -0500

    Added blueprint to server

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from blueprints import transactions_bp, persons_bp
+from blueprints import transactions_bp, persons_bp, reports_bp
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 
@@ -6,9 +6,13 @@ backend = Flask(__name__)
 
 backend.register_blueprint(persons_bp)
 backend.register_blueprint(transactions_bp)
+backend.register_blueprint(reports_bp)
 
 
 if __name__ == "__main__":
+    backend.secret_key = 'super secret key'
+    backend.config['SESSION_TYPE'] = 'filesystem'
+
     # backend.run(host='localhost', port=5555, debug=True)
     http_server = WSGIServer(('localhost', 5555), backend)
     http_server.serve_forever()
