06f6453fbb5639c9bfb299c8ac2dec9ded021ee0
Alex Tomberg <a.tomberg@gmail.com>
2020-01-08 22:42:49 -0500

    Split off debug server file

--- app/server_debug.py
+++ app/server_debug.py
@@ -1,6 +1,5 @@
 from blueprints import transactions_bp, persons_bp, reports_bp
 from flask import Flask
-from gevent.pywsgi import WSGIServer
 
 backend = Flask(__name__)
 backend.config.from_pyfile('config.py')
@@ -14,5 +13,4 @@ if __name__ == "__main__":
     backend.secret_key = 'super secret key'
     backend.config['SESSION_TYPE'] = 'filesystem'
 
-    http_server = WSGIServer(('localhost', 5555), backend)
-    http_server.serve_forever()
+    backend.run(host='localhost', port=5555, debug=True)
