d25a1f858624e9309581796d3d87c2e3c0718e8c
Alex Tomberg <a.tomberg@gmail.com>
2020-01-18 10:15:55 -0500

    Added automatic monthly backups of db file

--- app/models/db_session.py
+++ app/models/db_session.py
@@ -1,6 +1,17 @@
+import datetime
+import shutil
+
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+backup_path = Path('backups')
+backup_path.mkdir(exist_ok=True, parents=True)
+
+backup_path /= (datetime.date.today().replace(day=1) - datetime.timedelta(days=1)).strftime("%Y_%B.bak")
+if not backup_path.exists():
+    shutil.copyfile('donations.db', backup_path)
 
 engine = create_engine('sqlite:///donations.db')
 Session = scoped_session(sessionmaker(bind=engine))
