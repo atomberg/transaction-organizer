c5a03a7b7f4d13f693276afc75cfe9a1cdf0da3b
Alex Tomberg <a.tomberg@gmail.com>
2020-12-30 11:45:49 -0500

    Added pdf generation endpoints for tax receipts

--- app/blueprints/persons.py
+++ app/blueprints/persons.py
@@ -1,5 +1,9 @@
 from datetime import datetime
-from flask import Blueprint, request, render_template, flash, current_app as app
+
+from flask import Blueprint, current_app as app, flash, render_template, request, url_for
+
+from flask_weasyprint import HTML, render_pdf
+
 from models.db_session import Session
 from models.person import Person, get_persons
 
@@ -14,7 +18,7 @@ def display_newlines(value):
 @bp.route('/', methods=['GET'])
 def get_all():
     """Display all persons in a table."""
-    return render_template('person_table.html.j2', persons=get_persons())
+    return render_template('person_table.html.j2', persons=get_persons(), year=datetime.now().year)
 
 
 @bp.route('/form', methods=['GET'])
@@ -105,5 +109,36 @@ def receipt(person_id, year):
         receipt_date=datetime.now().strftime("%B %e, %Y"),
         name=p.full_name,
         address=p.address,
-        amount=sum([t.amount for t in p.transactions if t.date.year == year]),
+        amount=sum([t.amount for t in p.transactions if t.date.year == year and not t.receipt]),
     )
+
+
+@bp.route('/<int:person_id>/receipt/<int:year>/pdf', methods=['GET'])
+def receipt_pdf(person_id, year):
+    """Get a person's tax receipt by id in PDF form."""
+    return render_pdf(url_for('persons.receipt', person_id=person_id, year=year))
+
+
+@bp.route('/receipts/<int:year>/', methods=['GET'])
+def all_receipts_pdf(year):
+    """Get a person's tax receipt by id in PDF form."""
+    docs = {}
+    for p in get_persons():
+        if sum([t.amount for t in p.transactions if t.date.year == year and not t.receipt]) > 0:
+            docs[p.full_name] = HTML(url_for('persons.receipt', person_id=p.id, year=year)).render()
+
+    if len(docs) == 0:
+        return None
+
+    all_pages = []
+    final_doc = None
+    for full_name, doc in docs.items():
+        final_doc = doc
+        doc.pages[0].bookmarks = [(1, full_name, doc.pages[0].bookmarks[0][2], 'open')]
+        for page in doc.pages[1:]:
+            page.bookmarks = []
+        all_pages.extend(doc.pages)
+
+    final_doc.metadata.title = f'Tax receipts for {year}'
+    pdf = final_doc.copy(all_pages).write_pdf()
+    return app.response_class(pdf, mimetype='application/pdf')
