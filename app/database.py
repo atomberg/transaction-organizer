04ecae11ae8745bf64cf3d5d642c852648c31bba
Alex Tomberg <a.tomberg@gmail.com>
2022-02-12 23:10:12 -0500

    Fixed database threading warnings.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-engine = create_engine(settings.sqlalchemy_database_uri)
+engine = create_engine(settings.sqlalchemy_database_uri, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
